5861cda1f611a20c2811390ffb32b7e8056a274f
SolarSpectre <josephcaza32@gmail.com>
2025-04-29 15:46:28 -0500

    implementar un boton que muestre las fotos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,7 +9,38 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab3Page {
-
-  constructor() {}
+  showPhotos = false;
+  constructor(
+    public photoService: PhotoService,
+    private actionSheetController: ActionSheetController
+  ) {}
+  async loadPhotos(){
+    this.showPhotos = true;
+    await this.photoService.loadSaved();
+  }
+  async showActionSheet(photo: any, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.photoService.deletePicture(photo, position);
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            // Nothing to do, action sheet is automatically closed
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 
 }
